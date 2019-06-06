291f69f77153bd83559a5026943dee119e91b593
jeffrey_goh <jeffrey_goh@tech.gov.sg>
2019-06-07 07:58:43 +0800

    done with css architect

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const gulp = require('gulp'),
   postcss = require('gulp-postcss'),
   autoprefixer = require('autoprefixer'),
   cssvars = require('postcss-simple-vars'),
-  nested = require('postcss-nested');
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import');
 
 gulp.task('default', function() {
   console.log("Hooray - you created a Gulp task.");
@@ -16,17 +17,19 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
 
   watch('./app/index.html', function() {
+    // noinspection JSUnresolvedFunction
     gulp.start('html');
   });
 
   watch('./app/assets/styles/**/*.css', function() {
+    // noinspection JSUnresolvedFunction
     gulp.start('styles');
   });
 
